9d6f4de686960d58de5e1d24bcb8d936b46dfc74
nejukmaster <ttung102802@gmail.com>
2024-05-24 18:02:39 +0900

    migration-ver.2-1
    빌드되지 않는 버그 픽스

---
+++ Source/VClass/HostUI.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HostUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VCLASS_API UHostUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
