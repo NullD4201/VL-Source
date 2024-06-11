280a4aa2d7db86633976959947e02187cde7ab19
nejukmaster <ttung102802@gmail.com>
2024-06-11 11:28:41 +0900

    오푸스 압축 푸시면 lib파일 들어있습니다

---
+++ Source/VClass/VClassGameMode.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VClassGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VCLASS_API AVClassGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage) override;
+
+	virtual void BeginPlay() override;
+
+private:
+	bool ReadTextFile(FString FilePath, FString& FileContent);
+
+	TArray<FString> key_list;
+};
