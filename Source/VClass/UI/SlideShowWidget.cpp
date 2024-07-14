65c3d671edc040e344efc6014bf02a10377aa6be
NullD <nulld4201@gmail.com>
2024-07-15 02:32:49 +0900

    Up to 5.4.2

--- Source/VClass/UI/SlideShowWidget.cpp
+++ Source/VClass/UI/SlideShowWidget.cpp
@@ -3,64 +3,151 @@
 
 #include "SlideShowWidget.h"	
 
-void USlideShowWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
-{
-	Super::NativeTick(MyGeometry, InDeltaTime);
-
-	if (bIsDragging)
-	{
-		if (APlayerController* PlayerController = GetWorld()->GetFirstPlayerController())
-		{
-			float MouseX, MouseY;
-			PlayerController->GetMousePosition(MouseX, MouseY);
-
-			FVector2D ScreenPosition(MouseX, MouseY);
-			FVector2D WidgetPosition = MyGeometry.AbsoluteToLocal(ScreenPosition);
-
-			GEngine->AddOnScreenDebugMessage(-1, 2, FColor::Red, WidgetPosition.ToString());
-
-			if (_ImageSlideTemp)
-			{
-				_ImageSlideTemp->SetRenderTranslation(WidgetPosition);
-			}
-		}
-	}
-}
+#include "Blueprint/WidgetBlueprintLibrary.h"
+#include "Blueprint/WidgetTree.h"
+#include "Components/CanvasPanel.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Components/UniformGridPanel.h"
+#include "Components/UniformGridSlot.h"
+#include "VClass/Actor/ScreenActor.h"
 
 void USlideShowWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
 
-	if (ButtonTestSlide1)
+	TestButton = Cast<UButton>(GetWidgetFromName("Test"));
+	if (TestButton)
 	{
-		ButtonTestSlide1->OnPressed.AddDynamic(this, &USlideShowWidget::OnButtonPressed);
-		ButtonTestSlide1->OnReleased.AddDynamic(this, &USlideShowWidget::OnButtonReleased);
+		TestButton->OnClicked.AddDynamic(this, &USlideShowWidget::OnTestButtonClick);
+	}
+	TestButton2 = Cast<UButton>(GetWidgetFromName("Test2"));
+	if (TestButton2)
+	{
+		TestButton2->OnClicked.AddDynamic(this, &USlideShowWidget::OnTestButtonClick2);
 	}
 
-	bIsDragging = false;
+	ScreenActor1 = UGameplayStatics::GetActorOfClass(GetWorld(), AScreenActor::StaticClass());
 
-	// 입력 매핑 설정
-	if (UWorld* World = GetWorld())
+	DraggedImage = nullptr;
+	bIsDragging = false;
+}
+
+void USlideShowWidget::OnTestButtonClick()
+{
+	if (ScreenActor1)
 	{
-		APlayerController* PlayerController = World->GetFirstPlayerController();
-		if (PlayerController)
+		AScreenActor* ScreenActor = Cast<AScreenActor>(ScreenActor1);
+		if (ScreenActor)
 		{
-			PlayerController->InputComponent->BindAction("MouseLeftClick", IE_Released, this, &USlideShowWidget::OnMouseLeftClick);
+			ScreenActor->StartSlideTransition();
 		}
 	}
 }
 
-void USlideShowWidget::OnButtonPressed()
+void USlideShowWidget::OnTestButtonClick2()
 {
-	bIsDragging = true;
+	if (ScreenActor1)
+	{
+		AScreenActor* ScreenActor = Cast<AScreenActor>(ScreenActor1);
+		if (ScreenActor)
+		{
+			ScreenActor->StartSlideTransitionReverse();
+		}
+	}
 }
 
-void USlideShowWidget::OnButtonReleased()
+FVector2D GetImageSlotIndex(UUniformGridPanel* UniformGridPanel, UImage* Image)
 {
-	bIsDragging = false;
+	int IndexX = 0, IndexY = 0;
+	for (UWidget* WidgetComponent : UniformGridPanel->GetAllChildren())
+	{
+		UImage* ImageComponent = Cast<UImage>(WidgetComponent);
+
+		if (ImageComponent->GetName() == Image->GetName())
+		{
+			IndexX = Cast<UUniformGridSlot>(WidgetComponent->Slot)->GetRow();
+			IndexY = Cast<UUniformGridSlot>(WidgetComponent->Slot)->GetColumn();
+		}
+	}
+	return FVector2D(IndexX, IndexY);
 }
 
-void USlideShowWidget::OnMouseLeftClick()
+FReply USlideShowWidget::NativeOnMouseButtonDown(const FGeometry& Geometry, const FPointerEvent& MouseEvent)
 {
-	bIsDragging = false;
+    if (MouseEvent.GetEffectingButton() == EKeys::LeftMouseButton)
+    {
+        if (TestSlide1 && Geometry.IsUnderLocation(MouseEvent.GetScreenSpacePosition()))
+        {
+            bIsDragging = true;
+
+            DraggedImage = WidgetTree->ConstructWidget<UImage>(UImage::StaticClass(), TEXT("DraggedImage"));
+            DraggedImage->SetBrush(TestSlide1->Brush);
+            DraggedImage->SetRenderOpacity(0.5f);
+
+            if (RootCanvasPanel)
+            {
+                RootCanvasPanel->AddChild(DraggedImage);
+                UCanvasPanelSlot* DraggedImageSlot = Cast<UCanvasPanelSlot>(DraggedImage->Slot);
+                if (DraggedImageSlot)
+                {
+                	DragOffset = Geometry.AbsoluteToLocal(MouseEvent.GetScreenSpacePosition()) - DraggedImageSlot->GetPosition();
+
+                	UUniformGridPanel* GridPanel = Cast<UUniformGridPanel>(TestSlide1->GetParent());
+                	if (GridPanel)
+                	{
+                		UCanvasPanelSlot* CanvasSlot = Cast<UCanvasPanelSlot>(GridPanel->Slot);
+                		if (CanvasSlot)
+                		{
+                			int PosX = CanvasSlot->GetPosition().X + 30 * (GetImageSlotIndex(GridPanel, TestSlide1).Y);
+                			int PosY = CanvasSlot->GetPosition().Y + 45 * (GetImageSlotIndex(GridPanel, TestSlide1).X);
+                			DraggedImageSlot->SetPosition(FVector2D(PosX, PosY));
+
+                			double SizeX = (CanvasSlot->GetSize().X - 90) / 3;
+                			double SizeY = (CanvasSlot->GetSize().Y - 135) / 3;
+                			DraggedImageSlot->SetSize(FVector2D(SizeX, SizeY));
+                		}
+                	}
+                }
+            }
+
+            TestSlide1->SetRenderOpacity(0.5f);
+
+            FEventReply Reply;
+            return UWidgetBlueprintLibrary::CaptureMouse(Reply, this).NativeReply;
+        }
+    }
+	return Super::NativeOnMouseButtonDown(Geometry, MouseEvent);
+}
+
+FReply USlideShowWidget::NativeOnMouseMove(const FGeometry& Geometry, const FPointerEvent& MouseEvent)
+{
+	if (bIsDragging && DraggedImage)
+	{
+		FVector2D NewPosition = Geometry.AbsoluteToLocal(MouseEvent.GetScreenSpacePosition()) - DragOffset;
+		UCanvasPanelSlot* CanvasSlot = Cast<UCanvasPanelSlot>(DraggedImage->Slot);
+		if (CanvasSlot)
+		{
+			CanvasSlot->SetPosition(NewPosition);
+		}
+		return FReply::Handled();
+	}
+	return Super::NativeOnMouseMove(Geometry, MouseEvent);
+}
+
+FReply USlideShowWidget::NativeOnMouseButtonUp(const FGeometry& Geometry, const FPointerEvent& MouseEvent)
+{
+	if (bIsDragging)
+	{
+		bIsDragging = false;
+		if (DraggedImage)
+		{
+			DraggedImage->SetRenderOpacity(1.0f);
+			TestSlide1->SetRenderOpacity(0.5f);
+			DraggedImage = nullptr;
+		}
+		
+		FEventReply Reply;
+		return UWidgetBlueprintLibrary::ReleaseMouseCapture(Reply).NativeReply;
+	}
+	return Super::NativeOnMouseButtonUp(Geometry, MouseEvent);
 }
--- Source/VClass/UI/SlideShowWidget.h
+++ Source/VClass/UI/SlideShowWidget.h
@@ -5,6 +5,7 @@
 #include "../ProjectInfo.h"
 #include "Blueprint/UserWidget.h"
 #include "Components/Button.h"
+#include "Components/CanvasPanel.h"
 #include "Components/Image.h"
 #include "SlideShowWidget.generated.h"
 
@@ -17,26 +18,36 @@ class VCLASS_API USlideShowWidget : public UUserWidget
 	GENERATED_BODY()
 
 public:
-	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UButton* TestButton;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UButton* TestButton2;
 
 protected:
 	virtual void NativeConstruct() override;
+	virtual FReply NativeOnMouseButtonDown(const FGeometry& Geometry, const FPointerEvent& MouseEvent) override;
+	virtual FReply NativeOnMouseMove(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+	virtual FReply NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+
+	UPROPERTY(meta=(BindWidget))
+	class UImage* TestSlide1;
+
+	UFUNCTION()
+	void OnTestButtonClick();
+
+	UFUNCTION()
+	void OnTestButtonClick2();
 
 private:
 	UPROPERTY(meta = (BindWidget))
-	class UButton* ButtonTestSlide1;
-
-	UPROPERTY(meta = (BindWidget))
-	class UImage* _ImageSlideTemp;
+	UCanvasPanel* RootCanvasPanel;
+	
+	UImage* DraggedImage;
 
 	bool bIsDragging;
 
-	UFUNCTION()
-	void OnButtonPressed();
+	FVector2D DragOffset;
 
-	UFUNCTION()
-	void OnButtonReleased();
-
-	UFUNCTION()
-	void OnMouseLeftClick();
+	AActor* ScreenActor1;
 };
