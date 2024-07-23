31fa7e28f7280498ec7b7904a035e71a7ab6a5dd
nejukmaster <ttung102802@gmail.com>
2024-07-23 11:08:18 +0900

    electraPlay 수정

--- Source/VClass/Actor/VClassElectraPlayer.cpp
+++ Source/VClass/Actor/VClassElectraPlayer.cpp
@@ -12,7 +12,5 @@ AVClassElectraPlayer::AVClassElectraPlayer()
 
 void AVClassElectraPlayer::NativeSetURL_Implementation(const FString& url)
 {
-	BlueprintableSetURL(TEXT("https://www.youtube.com/watch?v=")+url+TEXT("&start_radio=1"));
+	BlueprintableSetURL(url);
 }
-
-
